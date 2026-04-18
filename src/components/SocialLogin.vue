e932d58cf487ae3549dd2606b8ddc6bd60f08bec
Hank Heiselbetz <hheiselbetz@gmail.com>
2026-04-18 12:24:14 -0500

    first page after log in, filtering position users for shifts

--- src/components/SocialLogin.vue
+++ src/components/SocialLogin.vue
@@ -1,6 +1,8 @@
 <script setup>
 import { ref, onMounted } from "vue";
 import AuthServices from "../services/authServices";
+import PositionServices from "../services/positionServices";
+import PositionUserServices from "../services/positionUserServices";
 import Utils from "../config/utils.js";
 import { useRouter } from "vue-router";
 
@@ -32,17 +34,36 @@ const handleCredentialResponse = async (response) => {
   let token = {
     credential: response.credential,
   };
-  await AuthServices.loginUser(token)
-    .then((response) => {
-      user.value = response.data;
-      Utils.setStore("user", user.value);
-      fName.value = user.value.fName;
-      lName.value = user.value.lName;
-      router.push({ name: "profile" });
-    })
-    .catch((error) => {
-      console.log("error", error);
-    });
+  try {
+    const loginRes = await AuthServices.loginUser(token);
+    user.value = loginRes.data;
+    Utils.setStore("user", user.value);
+    fName.value = user.value.fName;
+    lName.value = user.value.lName;
+
+    const userId = user.value.userId || user.value.user_id;
+    const [posRes, puRes] = await Promise.all([
+      PositionServices.getAll(),
+      PositionUserServices.getAll(),
+    ]);
+    const managerPositions = posRes.data.filter((p) => p.is_manager);
+    const myManagerPositionIds = puRes.data
+      .filter((pu) => Number(pu.user_id) === Number(userId))
+      .map((pu) => Number(pu.position_id));
+    const firstManagerPos = managerPositions.find((p) =>
+      myManagerPositionIds.includes(Number(p.position_id))
+    );
+
+    if (user.value.is_super_admin) {
+      router.push({ name: "users" });
+    } else if (firstManagerPos) {
+      router.push({ name: "manager-dashboard", query: { area: firstManagerPos.area_id } });
+    } else {
+      router.push({ name: "dashboard" });
+    }
+  } catch (error) {
+    console.log("error", error);
+  }
 };
 
 onMounted(() => {
